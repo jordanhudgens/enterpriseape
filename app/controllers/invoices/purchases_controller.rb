ced843aabb3c16163b8ae6ae4c831a6585bafcb6
Jordan Hudgens <jordan@hudgens.com>
2014-05-19 10:12:44 +0000

    deleting nested attributes in a rails application

--- app/controllers/invoices/purchases_controller.rb
+++ app/controllers/invoices/purchases_controller.rb
@@ -56,10 +56,16 @@ class Invoices::PurchasesController < ApplicationController
   # DELETE /purchases/1
   # DELETE /purchases/1.json
   def destroy
-    @purchase.destroy
-    respond_to do |format|
-      format.html { redirect_to purchases_url }
-      format.json { head :no_content }
+    @invoice = Invoice.find(params[:invoice_id])
+    @purchase = Purchase.find(params[:id])
+    title = @purchase.name
+    
+    if @purchase.destroy
+      flash[:notice] = "#{title} was deleted successfully."
+      redirect_to @invoice
+    else
+      flash[:error] = "There was an error deleting the purchase."
+      render :show
     end
   end
 
