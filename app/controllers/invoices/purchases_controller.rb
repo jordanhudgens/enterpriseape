13ca183ae984db0995df502883b9c83df35ee167
Jordan Hudgens <jordan@hudgens.com>
2014-05-20 02:09:00 +0000

    utilized strong parameters and the number_to_currency method to add the price to the purchases views

--- app/controllers/invoices/purchases_controller.rb
+++ app/controllers/invoices/purchases_controller.rb
@@ -77,6 +77,6 @@ class Invoices::PurchasesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def purchase_params
-      params.require(:purchase).permit(:name, :category, :quantity, :invoice_id)
+      params.require(:purchase).permit(:name, :category, :quantity, :invoice_id, :price)
     end
 end
